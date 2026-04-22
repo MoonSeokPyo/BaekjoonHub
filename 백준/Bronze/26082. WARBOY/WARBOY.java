1c061b8ed08528060db39a9ad4d75c87a19ad32e
Seokpyo Moon <moonseokpyo@gmail.com>
2026-04-22 16:24:58 +0900

    [Bronze V] Title: WARBOY, Time: 100 ms, Memory: 14444 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws Exception{
+		func_26082();
+		
+		br.close();
+		bw.close();
+	}
+	
+	static void func_26082() throws Exception {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		bw.write(b / a * 3 * c + "");
+	}
+}
