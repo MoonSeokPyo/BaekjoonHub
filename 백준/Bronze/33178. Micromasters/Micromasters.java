c92cd3043317c9bd19d15b78301eee1ad652a3c1
Seokpyo Moon <moonseokpyo@gmail.com>
2026-04-24 05:06:01 +0900

    [Bronze V] Title: Micromasters, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/33178. Micromasters/Micromasters.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws Exception{
+		func_33178();
+		
+		br.close();
+		bw.close();
+	}
+	
+	static void func_33178() throws Exception {
+		int n = Integer.parseInt(br.readLine());
+		bw.write(n / 10 + "");
+	}
+}
