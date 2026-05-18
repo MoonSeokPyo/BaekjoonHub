0014f20b5f9102b07304b613944b4569acfe5876
Seokpyo Moon <moonseokpyo@gmail.com>
2026-05-18 22:15:28 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 78 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
@@ -19,30 +19,39 @@ class Solution {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
 			int k = Integer.parseInt(st.nextToken());
-			int[][] puzzleR = new int[n][n];
-			int[][] puzzleC = new int[n][n];
+			int[][] board = new int[n][n];
 			for (int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < n; j++)
-					puzzleR[i][j] = puzzleC[i][j] = Integer.parseInt(st.nextToken());
+					board[i][j] = Integer.parseInt(st.nextToken());
 			}
 			int cnt = 0;
 			for (int i = 0; i < n; i++) {
-				for (int j = 1; j < n; j++) {
-					if (puzzleR[i][j] == 1) {
-						puzzleR[i][j] += puzzleR[i][j - 1];
-						if (puzzleR[i][j] == k && (j == n - 1 || puzzleR[i][j + 1] == 0))
+				int lenRow = 0;
+				int lenCol = 0;
+				for (int j = 0; j < n; j++) {
+					if (board[i][j] == 1) {
+						lenRow++;
+					} else {
+						if (lenRow == k)
 							cnt++;
+						lenRow = 0;
 					}
-					if (puzzleC[j][i] == 1) {
-						puzzleC[j][i] += puzzleC[j - 1][i];
-						if (puzzleC[j][i] == k && (j == n - 1 || puzzleC[j + 1][i] == 0))
+					if (board[j][i] == 1) {
+						lenCol++;
+					} else {
+						if (lenCol == k)
 							cnt++;
+						lenCol = 0;
 					}
 				}
+				if (lenRow == k)
+					cnt++;
+				if (lenCol == k)
+					cnt++;
 			}
 			sb.append('#').append(t).append(' ').append(cnt).append('\n');
 		}
 		bw.write(sb.toString());
-	}
+    }
 }
