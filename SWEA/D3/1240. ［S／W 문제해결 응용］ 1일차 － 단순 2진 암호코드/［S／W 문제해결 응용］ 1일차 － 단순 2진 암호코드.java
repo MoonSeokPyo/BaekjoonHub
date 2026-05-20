d875c50bff33cfdf91ff6f217752be0076ccfdc5
Seokpyo Moon <moonseokpyo@gmail.com>
2026-05-20 21:32:08 +0900

    [D3] Title: [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드, Time: 78 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.java
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.java
@@ -13,7 +13,6 @@ class Solution {
 	}
 	
 	static void func_1240() throws Exception {
-		char[][] code = new char[8][7];
 		int[] num = new int[8];
 		Context_1240 ct = new Context_1240();
 		StringBuilder sb = new StringBuilder();
@@ -22,13 +21,13 @@ class Solution {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
 			int m = Integer.parseInt(st.nextToken());
+			String code = null;
 			for (int i = 0; i < n; i++) {
 				String input = br.readLine();
 				if (input.contains("1")) {
 					for (int j = m - 1; j >= 0; j--) {
 						if (input.charAt(j) == '1') {
-							for (int k = 0; k < 56; k++)
-								code[(55 - k) / 7][(55 - k) % 7] = input.charAt(j - k);
+							code = input.substring(j - 55, j + 1);
 							break;
 						}
 					}
@@ -38,7 +37,7 @@ class Solution {
 				}
 			}
 			for (int i = 0; i < 8; i++)
-				num[i] = func_1240_decryption(ct, code[i]);
+				num[i] = func_1240_decryption(ct, code.substring(7 * i, 7 + 7 * i));
 			// (홀수 자리의 합 x 3) + (짝수 자리의 합)이 10의 배수
 			int result = 0;
 			for (int i = 0; i < 8; i++) {
@@ -74,9 +73,9 @@ class Solution {
 				"0001101", "0011001", "0010011", "0111101", "0100011", "0110001", "0101111", "0111011", "0110111", "0001011"
 		};
 	}
-	static int func_1240_decryption(Context_1240 ct, char[] code) {
+	static int func_1240_decryption(Context_1240 ct, String code) {
 		for (int i = 0; i < 10; i++) {
-			if (String.valueOf(code).equals(ct.code[i])) {
+			if (code.equals(ct.code[i])) {
 				return i;
 			}
 		}
