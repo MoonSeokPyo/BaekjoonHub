079d5cb8487c1b4170d9299ecc125be189ff4372
Seokpyo Moon <moonseokpyo@gmail.com>
2026-05-23 15:48:48 +0900

    [D3] Title: 쉬운 삼각형, Time: 84 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D3/26502. 쉬운 삼각형/쉬운 삼각형.java
+++ SWEA/D3/26502. 쉬운 삼각형/쉬운 삼각형.java
@@ -13,50 +13,53 @@ class Solution {
 	}
 	
 	static void func_26502() throws Exception {
-		StringBuilder sb = new StringBuilder();
-		int testcase = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= testcase; tc++) {
-			int n = Integer.parseInt(br.readLine());
-			int[][] arr = new int[n][2];
-			for (int i = 0; i < n; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				arr[i][0] = Integer.parseInt(st.nextToken());
-				arr[i][1] = Integer.parseInt(st.nextToken());
-			}
-			sb.append(func_26502_solve(arr, new int[3][2], 0, n, 0)).append('\n');
-		}
-		bw.write(sb.toString());
-	}
-	static int func_26502_solve(int[][] arr, int[][] choice, int idx, int n, int depth) {
-		if (idx == 3) {
-			Set<Integer> ySet = new HashSet<>();
-			Set<Integer> xSet = new HashSet<>();
-			int minY = Integer.MAX_VALUE;
-			int maxY = Integer.MIN_VALUE;
-			int minX = Integer.MAX_VALUE;
-			int maxX = Integer.MIN_VALUE;
-			for (int i = 0; i < 3; i++) {
-				ySet.add(choice[i][0]);
-				xSet.add(choice[i][1]);
-				minY = Math.min(minY, choice[i][0]);
-				maxY = Math.max(maxY, choice[i][0]);
-				minX = Math.min(minX, choice[i][1]);
-				maxX = Math.max(maxX, choice[i][1]);
-			}
-			if (ySet.size() == 2 && xSet.size() == 2)
-				return (maxY - minY) * (maxX - minX);
-			return 0;
-		}
-		if (depth == n)
-			return 0;
-		
-		int max = 0;
-		max = Math.max(max, func_26502_solve(arr, choice, idx, n, depth + 1));
-		if (idx < 3) {
-			choice[idx][0] = arr[depth][0];
-			choice[idx][1] = arr[depth][1];
-			max = Math.max(max, func_26502_solve(arr, choice, idx + 1, n, depth + 1));
-		}
-		return max;
+		 StringBuilder sb = new StringBuilder();
+
+    int testcase = Integer.parseInt(br.readLine());
+
+    for (int tc = 1; tc <= testcase; tc++) {
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] points = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            points[i][0] = Integer.parseInt(st.nextToken());
+            points[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            int xLen = 0;
+            int yLen = 0;
+
+            for (int j = 0; j < n; j++) {
+
+                if (points[i][0] == points[j][0]) {
+                    xLen = Math.max(
+                        xLen,
+                        Math.abs(points[i][1] - points[j][1])
+                    );
+                }
+
+                if (points[i][1] == points[j][1]) {
+                    yLen = Math.max(
+                        yLen,
+                        Math.abs(points[i][0] - points[j][0])
+                    );
+                }
+            }
+
+            maxArea = Math.max(maxArea, xLen * yLen);
+        }
+
+        sb.append(maxArea).append('\n');
+    }
+
+    bw.write(sb.toString());
 	}
 }
