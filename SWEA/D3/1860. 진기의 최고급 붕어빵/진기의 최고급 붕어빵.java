7f38ddcc3ca8df2b949df2a47797a7cc2a84b18a
Seokpyo Moon <moonseokpyo@gmail.com>
2026-07-24 00:45:42 +0900

    [D3] Title: 진기의 최고급 붕어빵, Time: 158 ms, Memory: 37,452 KB -BaekjoonHub

--- SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.java
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.java
@@ -22,24 +22,21 @@ class Solution {
 			int K = Integer.parseInt(st.nextToken());
 			st = new StringTokenizer(br.readLine());
 			
-			int[] t = new int[11_111 + 1];
+			TreeMap<Integer, Integer> map = new TreeMap<>();
 			for (int i = 0; i < N; i++) {
 				int time = Integer.parseInt(st.nextToken());
-				t[time]++;
+				map.put(time, map.getOrDefault(time, 0) + 1);
 			}
-			
+			boolean result = true;
 			int cnt = 0;
-			boolean result = t[0] == 0 ? true : false;
-			if (result)
-				for (int i = 1; i <= 11_111; i++) {
-					if (i % M == 0)
-						cnt += K;
-					cnt -= t[i];
-					if (cnt < 0) {
-						result = false;
-						break;
-					}
+			for (int time : map.keySet()) {
+				cnt += map.get(time);
+				int bread = (time / M) * K;
+				if (cnt > bread) {
+					result = false;
+					break;
 				}
+			}
 			
 			sb.append('#').append(tc).append(' ').append(result ? "Possible" : "Impossible").append('\n');
 		}
